35844e29bcea82cad7d5186256571d27902a70f0
Biller17 <abiller95@gmail.com>
2016-02-26 10:48:08 -0600

    Creado Inicializacion units

---
+++ unit.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class unit : MonoBehaviour {
+	public float atk, atk_range, view_range;
+	public float def_melee, def_cavalry, def_range;
+	public float vel_walk, vel_atk;
+	public float hp;
+	public string civ;
+	
+	
+	// Use this for initialization
+	void Start () {
+		if (this.gameObject.tag == "melee") {
+			Debug.Log ("atributos melee asignados");
+			hp = 10.0f;
+			atk = 3.0f;
+			atk_range = 2.0f;
+			view_range = 7.0f;
+			def_melee = 1.0f;
+			def_range = 1.2f;
+			def_cavalry = 0.5f;
+			vel_walk = 1.0f;
+			vel_atk = 2.0f;
+		}
+		if (this.gameObject.tag == "range") {
+			Debug.Log ("atributos range asignados");
+			hp = 5.0f;
+			atk = 2.0f;
+			atk_range = 5.0f;
+			view_range = 10.0f;
+			def_melee = 0.5f;
+			def_range = 1.0f;
+			def_cavalry = 1.0f;
+			vel_walk = 2.0f;
+			vel_atk = 3.0f;
+
+		}
+		if (this.gameObject.tag == "cavalry") {
+			Debug.Log ("atributos cavalry asignados asignados");
+			hp = 15.0f;
+			atk = 5.0f;
+			atk_range = 3.0f;
+			view_range = 10.0f;
+			def_melee = 1.0f;
+			def_range = 0.5f;
+			def_cavalry = 1.0f;
+			vel_walk = 4.0f;
+			vel_atk = 2.0f;
+		}
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (hp == 0) {
+			Destroy(this.gameObject);
+			//agregar animacion de muerte de unidad
+		}
+
+	
+	}
+}
