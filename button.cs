82a7f8ba854c5bb006a1c700064ab006c0a76847
Biller <abiller95@gmail.com>
2016-03-31 08:02:35 -0600

    Genera boton de script

---
+++ button.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+	public GameObject pyramidButton;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		pyramidButton = Instantiate(Resources.Load("pyramidB")) as GameObject; 
+		Instantiate (pyramidButton, transform.position = Vector3.zero , Quaternion.identity);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
