e87ee1c312aaa919b87935798dadd5b7939c47ae
Biller <abiller95@gmail.com>
2016-04-06 18:29:36 -0500

    Botones

---
+++ buttonEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonEvent : MonoBehaviour {
+
+	//Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		onMouseEnter ();
+	}
+
+	void onMouseEnter()
+	{
+		Debug.Log ("suck my dick");
+		if (Input.GetMouseButtonDown (0)) {
+			if (this.gameObject.name == "pyramidButton") {
+				GameObject pyramid = Instantiate (Resources.Load ("egypt.building.pyramid.classic")) as GameObject; 
+				//Instantiate (pyramid, transform.position = Vector3.zero, Quaternion.identity);
+			}
+		}
+	}
+}
